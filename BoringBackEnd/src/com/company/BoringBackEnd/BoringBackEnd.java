18e3d9d37825079f3bd7b14dc7538a07ae0045f6
Michael Kerr <mikekerr247@hotmail.com>
2017-11-11 16:42:21 -0500

    add stubs

---
+++ BoringBackEnd/src/com/company/BoringBackEnd/BoringBackEnd.java
@@ -0,0 +1,103 @@
+package com.company.BoringBackEnd;
+
+import java.util.*;
+
+public class BoringBackEnd {
+
+    public static ArrayList<String[]> oldMasterAccounts = null;
+    public static ArrayList<String[]> newMasterAccounts = null;
+    public static String[] summaryLine = null;
+    public static String oldMasterAccountsFilename;
+    public static String newMasterAccountsFilename;
+    public static String validAccountsFilename;
+    public static String transactionSummaryFilename;
+
+//katherine
+    public static void readOldMasterAccounts(){
+
+    }
+//katherine
+    public static void readAllSummaryFiles(){
+
+    }
+//katherine
+    public static void readSummaryFile(){
+
+    }
+//katherine
+    public static void parseSummaryLine(){
+
+    }
+//katherine
+    public static void checkTransactionCode(){
+
+    }
+//john
+    public static void createAccount(){
+
+    }
+//john
+    public static void deleteAccount(){
+
+    }
+
+    public static void withdraw(){
+
+    }
+
+    public static void deposit(){
+
+    }
+
+    public static void transfer(){
+
+    }
+//katherine
+    public static boolean isBalanceNegative(){
+        return true;
+    }
+//katherine
+    public static boolean accountNumMatchesName(){
+        return true;
+    }
+//john
+    public static boolean isBalanceZero(){
+        return true;
+    }
+//john
+    public static boolean masterAccountListContains(String accNum){
+        return true;
+    }
+
+    public static boolean checkValidAccount(){
+        return true;
+    }
+
+    public static boolean checkValidName(){
+        return true;
+    }
+
+    public static boolean checkValidAmount(){
+        return true;
+    }
+//john
+    public static void writeNewMasterAccounts(){
+
+    }
+//john
+    public static void writeNewValidAccounts(){
+
+    }
+//john
+    public static void fatalError(){
+
+    }
+
+    public static void errorMessage(String message){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
